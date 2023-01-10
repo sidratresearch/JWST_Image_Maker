536e42b2c20b971249e865cc6e5ea9ea1a337eda
henrywhite2727 <henrywhite@sidratresearch.com>
2023-01-10 10:33:07 -0500

    removing .gitattributes

---
+++ plotting2.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+def plot_data(processed_data,filename,save_image):
+    """_summary_
+
+    Args:
+        processed_data (np.array): Processed data, ideally 2D but can also be 3D. Remember that this will NOT be a multi-row/2-column dataset
+    """
+    #Add some code here later that makes it look pretty and (maybe) customizes the plotting procedure based on what the image contains
+
+        # showing just the first slice
+    for i in range(len(processed_data[0,0,:])): 
+        img = processed_data[:,:,i]
+        vmin,vmax=np.percentile(img.flatten(), [12,99])  #12 and 99 were determined by trial and error
+        plt.figure(i,figsize=(5,5),dpi=175)
+        plt.imshow(img, vmin=vmin, vmax=vmax, cmap='bone')
+        #saving image if desired by user
+        if save_image==True:
+            name = os.path.splitext(filename[i])[0].lower()
+            new_ext='.pdf'
+            plt.savefig(name+new_ext,format='pdf',dpi=1200,bbox_inches='tight')
+        plt.show()
+
+
+
+    pass
+
--- processing.py
+++ processing.py
@@ -6,20 +6,16 @@ def process_file(file_data):
     """_summary_
 
     Args:_type_
-        file_data (FITS data structure): raw FITS file from JWST
+        file_data (np.array): array containing data from JWST
 
     Returns:
         processed_data(np.array): _description_
     """
-    # showing just the first slice
-    img = file_data[1].data
-    vmin,vmax=np.percentile(img.flatten(), [12,99])
-    plt.figure(figsize=(5,5),dpi=175)
-    plt.imshow(img, vmin=vmin, vmax=vmax, cmap='bone')
-    plt.show()
+
 
     # TODO: auto adjust based on distribution, account for all slices
     #       account for biases
 
-    processed_data=file_data+5 #hahahahahaha
+    processed_data=file_data
+
     return processed_data
