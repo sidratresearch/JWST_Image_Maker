a219fcaca7825ffdc28873bdc9316e22fe7e896f
hansenjiang <hansen@sidratresearch.com>
2023-01-13 05:09:10 -0500

    some plot tests

--- JWST_IMAGE_MAKER/processing.py
+++ JWST_IMAGE_MAKER/processing.py
@@ -23,23 +23,29 @@ def curves(images):
     return adjusted
 
 
-def curve(image):
+def curve(image, scale=255.0, mean_div=8.0, stddev_div=16.0):
     """Adjusts the brightness of an image so that the distribution is
     well-spread in the context of being visualized by a 1D colour space.
 
     Currently, the optimum brightness is so that the mean of the distribution is
-    at 1/4th of the brightness spectrum, the standard deviation is 1/16th of
+    at 1/8th of the brightness spectrum, the standard deviation is 1/16th of
     the spectrum, and the max and min are at the max and min of the spectrum.
+
+    Intuitively, the greater the mean, the brighter the image, and the greater
+    the standard deviation, the more contrast the image has.
 
     Args:
         image (np.array): 2D image containing flux values to be adjusted
+        scale (float, optional): max brightness value, defaults to 255.0
+        mean_div (float, optional): divisions to scale to equal mean, defaults to 8.0
+        stddev_div (float, optional): divisions to scale to equal stddev, defaults to 16.0
 
     Returns:
         np.array: adjusted image with brightness "normalized"
     """
     # Setting the desired image constants
-    scale = 255.0
-    mu_curve, sigma_curve = scale / 8, scale / 16
+    scale = scale
+    mu_curve, sigma_curve = scale / mean_div, scale / stddev_div
 
     # Scaling and clipping the image then calculating mean and standard deviation
     scaled = np.clip(scale * image / np.max(image), 0.0, scale)
@@ -63,17 +69,6 @@ def curve(image):
             curved, np.sign(mu / mu_curve - 1.0) * np.abs(mu_curve / mu) * mu_curve
         )
         mu = np.mean(curved.flatten())
-
-    plt.figure(figsize=(15, 8))
-    plt.subplot(121)
-    plt.imshow(scaled, cmap="gray")
-    plt.subplot(122)
-    plt.imshow(curved, cmap="gray")
-    plt.show()
-
-    plt.hist(scaled.flatten(), bins=100, alpha=0.5)
-    plt.hist(curved.flatten(), bins=100, alpha=0.5)
-    plt.show()
 
     return curved
 
@@ -152,5 +147,47 @@ def process_file(data):
     return adjusted
 
 
+# TODO DELETE, TESTING AREA
+
+
+def plot_test(sp, image, md, sd):
+    curved = curve(image, mean_div=md, stddev_div=sd)
+    plt.subplot(sp)
+    plt.imshow(curved, cmap="bone")
+    plt.title(
+        f"mean={np.mean(curved.flatten()):.1f} ({md:.0f} divisions) \n stddev={np.std(curved.flatten()):.1f} ({sd:.0f} divisions)"
+    )
+    return curved
+
+
 data = get_files("/JWST_IMAGE_MAKER/data/test_data_eagle.fits")
-curve(data[0][0])
+
+image = data[0][0]
+scale = 255.0
+scaled = np.clip(scale * image / np.max(image), 0.0, scale)
+
+plt.figure(figsize=(16, 8))
+plt.subplot(241)
+plt.imshow(scaled, cmap="bone")
+plt.title(
+    f"mean={np.mean(scaled.flatten()):.2f} stddev={np.std(scaled.flatten()):.2f}\noriginal"
+)
+c1 = plot_test(242, image, 9.0, 12.0)
+c2 = plot_test(243, image, 8.0, 13.0)
+c3 = plot_test(244, image, 7.0, 14.0)
+c4 = plot_test(245, image, 6.0, 15.0)
+c5 = plot_test(246, image, 5.0, 16.0)
+c6 = plot_test(247, image, 4.0, 17.0)
+c7 = plot_test(248, image, 3.0, 18.0)
+plt.show()
+
+# plt.hist(scaled.flatten(), bins=100, alpha=0.2)
+# plt.hist(c1.flatten(), bins=100, alpha=0.2)
+# plt.hist(c2.flatten(), bins=100, alpha=0.2)
+# plt.hist(c3.flatten(), bins=100, alpha=0.2)
+# plt.hist(c4.flatten(), bins=100, alpha=0.2)
+# plt.hist(c5.flatten(), bins=100, alpha=0.2)
+# plt.hist(c6.flatten(), bins=100, alpha=0.2)
+# plt.hist(c7.flatten(), bins=100, alpha=0.2)
+# plt.yscale("log")
+# plt.show()
