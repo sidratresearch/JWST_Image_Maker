63ebc9836bc8494aa4ca77c1f6f399e9587c40c3
henrywhite2727 <henrywhite@sidratresearch.com>
2023-01-16 12:01:35 -0500

    making into a function and adding test file

---
+++ tests/test_get_query_data.py
@@ -0,0 +1,6 @@
+from JWST_IMAGE_MAKER import make_image
+from JWST_IMAGE_MAKER.Astroquery_jwst import get_query_data
+
+def test_get_query_data():
+    astro_object='M16'
+    get_query_data(astro_object)
