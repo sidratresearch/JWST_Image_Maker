ab8f1c7be5b822e6c38d5663077c8367a9b4d165
henrywhite2727 <henrywhite@sidratresearch.com>
2023-01-26 16:08:17 -0500

    improving documentation

--- JWST_IMAGE_MAKER/Querying.py
+++ JWST_IMAGE_MAKER/Querying.py
@@ -7,13 +7,14 @@ import os
 
 
 def get_query_data(obj_name):
-    """_summary_
+    """This is the main function used to query the MAST repository for any JWST data for a given astronomical object. It will 
+    download the files into a directory with the same name as obj_nam in the directory (Query Data)
 
     Args:
-        object_name (str): _description_
+        obj_name (str): The name of the astronomical object being queried. Ex. 'M16','Orion Nebula', etc.
 
     Returns:
-        _type_: _description_
+        list: A list of the filenames downloaded through this querying
     """
     query_result = query(obj_name)
     observation_IDs = get_observation_IDs(query_result)
@@ -38,7 +39,7 @@ def query(object_name):
     Args:
         object_name (str): Name of the astronomical object given by the user
 
-    Output:
+    Returns:
         query result (Astropy Table): contains all of the information regarding data on that object
     """
     # if RA_dec==True:
@@ -62,16 +63,13 @@ def query(object_name):
 
 
 def get_observation_IDs(query_result):
-    """This function downloads the 2D image .fits file ('...i2d.fits') for JWST NIRCAM or MIRI data gathered in the query.
-    This function is only looking at MIRI data as NIRCAM files are too large (>2 GB)
+    """Sorts through the query result and saves a list of observation IDs for MIRI or NIRCAM data taken by the JWST while looking at the astronomical object desired by the user. 
 
     Args:
-        query result (Astropy Table): contains all of the information regarding data on that object
-        object_name (str): Name of the astronomical object given by the user
+        query_result (Astropy Table): all files flagged by MAST for being affiliated with the desired astronomical object
 
     Returns:
-        Nothing. However, the 2D image .fits files for a given astronomical object is saved in a folder with the following path: ./Query_Data/object_name/'{filename}...i2d.fits'
-
+        list: a list of all relevant observation IDs
     """
     # All relevant observation ID's are saved in the miri_obsid list so they can be downloaded. The first choice is NIRCAM data as it contains the most high definition data. If
     # NIRCAM data is unavailable, MIRI data is searched for
@@ -89,7 +87,6 @@ def get_observation_IDs(query_result):
 
     first_ra = 0  # this initialization is necessary to ensure the RA and dec of the first fits file selected is not supercded by subsequent iterations in the for loop
     first_dec = 0
-    first_position_bnd_cntr = 0
 
     for i in range(len(query_result)):
         if i % 10 == 0:
@@ -100,7 +97,7 @@ def get_observation_IDs(query_result):
 
         if (
             instrument_name == "NIRCAM" and MIRI == False and 1 == 0
-        ):  # and 1==0:  # comment out the 1==0 part if you want to use NIRCAM data
+        ):  # comment out the 1==0 part if you want to use NIRCAM data
             NIRCAM = True
             if filter not in filters_loaded:
                 obs_ids, filters_loaded = check_data_coords(
@@ -128,15 +125,15 @@ def check_data_coords(
     """ Checking RA, Dec and position bounds center of image taken to ensure the different .fits files gathered are for the same region with a galaxy
 
     Args:
-        first_ra (_type_): _description_
-        first_dec (_type_): _description_
-        query_result (_type_): _description_
-        i (int): _description_
-        obs_ids (list): _description_
-        filters_loaded (list): _description_
+        first_ra (float): initialization of variable used to make sure check is occurring properly
+        first_dec (float): initialization of variable used to make sure check is occurring properly
+        query_result (Astropy Table): full result of MAST query
+        i (int): iterative variable
+        obs_ids (list): list of relevant observation IDs
+        filters_loaded (list): list of wavelength filters associated with the observation IDs.
 
     Returns:
-        _type_: _description_
+        list,list: list of relevant observation IDs, list of the wavelength filter associated with those observations
     """
 
     if first_ra == 0 and first_dec == 0:
@@ -168,11 +165,19 @@ def check_data_coords(
 
 
 def download_files(obs_ids, object_name):
-    # Downloading relevant fits files from  observation ID's
-    # The relevant file in the product_list folder is the one containing i2d at the end
-    # (these are 2D images, for more info see https://jwst-pipeline.readthedocs.io/en/stable/jwst/data_products/science_products.html#i2d)
+    """This function downloads the 2D image .fits file ('...i2d.fits') for JWST NIRCAM or MIRI data gathered in the query.
+    This function is only looking at MIRI data as NIRCAM files are too large (>2 GB)
 
-    #   ****    Right now, this code is just looking at the first observation ID, this should be updated later to sort through relevant folders  *****
+    Args:
+        obs_ids (list): contains all of the observation IDs for relevant data
+        object_name (str): Name of the astronomical object given by the user
+
+    Returns:
+        void: Nothing. However, the 2D image .fits files for a given astronomical object is saved in a folder with the following path
+        
+        ./Query_Data/object_name/'{filename}...i2d.fits'
+
+    """
 
     # Creating a new folder for 'i2d.fits' data from query
     newpath = "./Query_Data/" + object_name
